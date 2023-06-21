38e9f6cbe6e516af592d0b56173b839443fd5d74
fernando <fernando.babonoyaba@gmail.com>
2023-06-21 11:04:17 -0600

    custom modifier compleated

--- SwiftUI/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ SwiftUI/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -22,12 +22,28 @@ struct ContentView: View {
                     .font(useLargeFont ? .largeTitle : .subheadline)
             }
             Toggle("change font to large", isOn: $useLargeFont)
+            Spacer()
         }
-        
+        .padding(50)
+        .changeBackground()
 
     }
 }
 
+struct ChangeBacground: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .background(
+                LinearGradient(colors: [.black, .yellow, .black], startPoint: .top, endPoint: .bottom))
+    }
+    
+    
+}
+extension View {
+    func changeBackground() -> some View {
+        modifier(ChangeBacground())
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
