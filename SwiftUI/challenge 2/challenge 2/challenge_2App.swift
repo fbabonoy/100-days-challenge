9783c5187b53a0aae443144a3bbb6abc9851981b
fernando <fernando.babonoyaba@gmail.com>
2023-06-24 10:31:59 -0600

    start setup challenge 2

---
+++ SwiftUI/challenge 2/challenge 2/challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_2App.swift
+//  challenge 2
+//
+//  Created by fernando babonoyaba on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
