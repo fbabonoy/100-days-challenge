6851668e4bd0ef759c3514209e1df879d80b8eb9
fernando <fernando.babonoyaba@gmail.com>
2023-07-01 14:25:21 -0600

    finish project 5

--- SwiftUI/WordScramble/WordScramble/ContentView.swift
+++ SwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -9,20 +9,126 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let people = ["Finn", "Leia", "Luke", "Rey"]
-
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    
     var body: some View {
-        List(people, id: \.self) { name in
-            HStack{
-                Text("dynamic people \(name)")
-                Spacer()
-                Button("print name") {
-                    print(name)
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+
+                }
+
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
                 }
             }
-            
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("Ok", role: .cancel) {
+                    
+                }
+            } message: {
+                Text(errorMessage)
+            }
+
+
         }
     }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
+
+        // extra validation to come
+//        guard isOriginal(word: answer) else {
+//            wordError(title: "not original", message: "The word is not original. \n try again")
+//            return
+//
+//        }
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let game = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let gameWord = try? String(contentsOf: game) {
+                let list = gameWord.components(separatedBy: "\n")
+                rootWord = list.randomElement() ?? "No Word"
+            }
+        }
+        
+        fatalError("the word could not be loaded")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let check = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        
+        let look = check.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "English")
+        return look.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
